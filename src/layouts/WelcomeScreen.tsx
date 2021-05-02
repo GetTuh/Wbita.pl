b6daeee4b1f60bbe6a17482b53b22e9f6fecb3ba
GetTuh <jakubniemczyk97@gmail.com>
2021-05-02 21:45:40 +0200

    :building_construction: Header and Hero placeholders

---
+++ src/layouts/WelcomeScreen.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+const WelcomeScreen = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+export default WelcomeScreen;
