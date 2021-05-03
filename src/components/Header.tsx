44c607e0a2911bcbeeb46ea76bfb41a4b0166ee5
GetTuh <ktosspl@gmail.com>
2021-05-03 21:39:19 +0200

    :bug: eslint compliant

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,25 @@ const Header = () => (
         <div>
             <link
                 rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+                // href={()=>"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"}
             />
             <header className="text-gray-100 bg-gray-900 body-font shadow w-full">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                        <a className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
+                        <div className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
                             About
-                        </a>
-                        <a className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
+                        </div>
+                        <div className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
                             Products
-                        </a>
-                        <a className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
+                        </div>
+                        <div className="mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
                             Investor Relations
-                        </a>
-                        <a className="hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
+                        </div>
+                        <div className="hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600">
                             Contact
-                        </a>
+                        </div>
                     </nav>
-                    <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-center mb-4 md:mb-0">
+                    <div className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-center mb-4 md:mb-0">
                         <img
                             src="https://icons.iconarchive.com/icons/google/noto-emoji-activities/1024/52707-party-popper-icon.png"
                             style={{
@@ -34,14 +34,14 @@ const Header = () => (
                             alt="logo"
                         />
                         <span className="ml-3 text-xl">Wbita.pl</span>
-                    </a>
+                    </div>
                     <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-                        <a
-                            href="https://www.buymeacoffee.com/pazlydev"
+                        <div
+                            
                             className="bg-indigo-700 hover:bg-indigo-500 text-white ml-4 py-2 px-3 rounded-lg"
                         >
                             Donate
-                        </a>
+                        </div>
                     </div>
                 </div>
             </header>
--- src/layouts/WelcomeScreen.tsx
+++ src/layouts/WelcomeScreen.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
-const WelcomeScreen = () => {
-    return (
+const WelcomeScreen = () => (
         <div>
             <Header />
             <Hero />
         </div>
     )
-}
+
 export default WelcomeScreen
